9ade622ab8af5fbafe810fab82d3c2750ef59089
Siddharth <siddharth@Siddharths-MacBook-Air.local>
2025-08-26 21:06:37 +0530

    Added Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Login from './Pages/Login/Login'
 
 const App = () => {
   return (
     <div>
       this is our hackathon project
+      <Login/>
     </div>
   )
 }
